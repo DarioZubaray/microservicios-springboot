691efc1beaf9eae034710cba497e789fa4db4a21
dario.zubaray <dzubaray@imagosur.com>
2020-03-09 10:05:06 -0300

    Agrgando balanceo de carga al servicio con restTemplate, se simplifica la inyeccion del valor del puerto de microservicio de productos

--- springboot-servicio-items/src/main/java/com/github/dariozubaray/springboot/items/models/service/ItemServiceImpl.java
+++ springboot-servicio-items/src/main/java/com/github/dariozubaray/springboot/items/models/service/ItemServiceImpl.java
@@ -6,10 +6,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.github.dariozubaray.springboot.items.models.Item;
 import com.github.dariozubaray.springboot.items.models.Producto;
 
+@Service("ItemRestTemplate")
 public class ItemServiceImpl implements ItemService {
 
     @Autowired
@@ -17,7 +19,7 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public List<Item> listar() {
-        Producto[] productos = restTemplate.getForObject("http://localhost:8001/listar", Producto[].class);
+        Producto[] productos = restTemplate.getForObject("http://servicio-productos/listar", Producto[].class);
         List<Producto> list = Arrays.asList(productos);
         return list.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
     }
@@ -26,7 +28,7 @@ public class ItemServiceImpl implements ItemService {
     public Item detalle(Long id, Integer cantidad) {
         Map<String, String> pathVariables = new HashMap<>();
         pathVariables.put("id", id.toString());
-        Producto producto = restTemplate.getForObject("http://localhost:8001/ver/{id}", Producto.class, pathVariables);
+        Producto producto = restTemplate.getForObject("http://servicio-productos/ver/{id}", Producto.class, pathVariables);
         return new Item(producto, cantidad);
     }
 
--- springboot-servicio-productos/src/main/java/com/github/dariozubaray/springboot/productos/models/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/github/dariozubaray/springboot/productos/models/controllers/ProductoController.java
@@ -3,6 +3,7 @@ package com.github.dariozubaray.springboot.productos.models.controllers;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ public class ProductoController {
 
     @Autowired
     private Environment env;
+    @Value("${server.port}")
+    private Integer port;
 
     @Autowired
     private IProductoService productoService;
@@ -22,7 +25,8 @@ public class ProductoController {
     @GetMapping("/listar")
     public List<Producto> listar() {
         return productoService.findAll().stream().map(p -> {
-            p.setPuerto(Integer.parseInt(env.getProperty("local.server.port")));
+//            p.setPuerto(Integer.parseInt(env.getProperty("local.server.port")));
+            p.setPuerto(port);
             return p;
         }).collect(Collectors.toList());
     }
@@ -30,7 +34,8 @@ public class ProductoController {
     @GetMapping("/ver/{id}")
     public Producto detalle(@PathVariable Long id) {
         Producto p = productoService.findById(id);
-        p.setPuerto(Integer.parseInt(env.getProperty("local.server.port")));
+//        p.setPuerto(Integer.parseInt(env.getProperty("local.server.port")));
+        p.setPuerto(port);
         return p;
     }
 }
