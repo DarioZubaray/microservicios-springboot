99ee672bcddf37801356e8d928dc48daa9e9b097
dario.zubaray <dzubaray@imagosur.com>
2020-03-10 08:38:31 -0300

    conectando nuestros servicios como clientes de Eureka

--- springboot-servicio-items/src/main/java/com/github/dariozubaray/springboot/items/SpringbootServicioItemsApplication.java
+++ springboot-servicio-items/src/main/java/com/github/dariozubaray/springboot/items/SpringbootServicioItemsApplication.java
@@ -2,10 +2,10 @@ package com.github.dariozubaray.springboot.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "servicio-productos")
+@EnableEurekaClient // No es obligacion anotar la app como cliente, si esta en el path lo va a ser
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemsApplication {
--- springboot-servicio-productos/src/main/java/com/github/dariozubaray/springboot/productos/SpringbootServicioProductosApplication.java
+++ springboot-servicio-productos/src/main/java/com/github/dariozubaray/springboot/productos/SpringbootServicioProductosApplication.java
@@ -2,7 +2,9 @@ package com.github.dariozubaray.springboot.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient // No es obligacion anotar la app como cliente, si esta en el path lo va a ser
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
 
