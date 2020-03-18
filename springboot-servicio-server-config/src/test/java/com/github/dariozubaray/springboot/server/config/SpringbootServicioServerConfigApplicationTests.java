e08a915d252b9d333adee12915c3873662f34df2
dario zubaray <dzubaray@gmail.com>
2020-03-18 17:27:12 -0300

    creando el microservicio del servidor de configuracion

---
+++ springboot-servicio-server-config/src/test/java/com/github/dariozubaray/springboot/server/config/SpringbootServicioServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dariozubaray.springboot.server.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
