c2424497a492b9a71726733d9cd5daec7518964c
dario zubaray <dzubaray@gmail.com>
2020-03-20 11:10:53 -0300

    configurando profiles de servicio items

--- springboot-servicio-items/src/main/java/com/github/dariozubaray/springboot/items/models/controllers/ItemController.java
+++ springboot-servicio-items/src/main/java/com/github/dariozubaray/springboot/items/models/controllers/ItemController.java
@@ -1,21 +1,39 @@
 package com.github.dariozubaray.springboot.items.models.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.github.dariozubaray.springboot.items.models.Item;
+import com.github.dariozubaray.springboot.items.models.Producto;
 import com.github.dariozubaray.springboot.items.models.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
 
+    private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     @Qualifier("ItemServiceFeign")
     private ItemService itemService;
+
+    @Value("${configuracion.texto}")
+    private String texto;
 
     @GetMapping("/listar")
     public List<Item> listar() {
@@ -29,6 +47,31 @@ public class ItemController {
     }
 
     public Item metodoAlternativo(Long id, Integer cantidad) {
-        return new Item();
+        Item item = new Item();
+        Producto producto = new Producto();
+
+        item.setCantidad(cantidad);
+        producto.setId(id);
+        producto.setNombre("Producto de prueba");
+        producto.setPrecio(0.00);
+        item.setProducto(producto);
+
+        return item;
+    }
+
+    @GetMapping("/obtener-config")
+    public ResponseEntity<?> obtenerConfiguracion(@Value("${server.port}") String puerto) {
+        log.debug("Texto: {}", texto);
+
+        Map<String, String> respuestaJson = new HashMap<>();
+        respuestaJson.put("texto", texto);
+        respuestaJson.put("puerto", puerto);
+
+        if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev") ) {
+            respuestaJson.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+            respuestaJson.put("autor.email", env.getProperty("configuracion.autor.email"));
+        }
+
+        return new ResponseEntity<Map<String, String>>(respuestaJson, HttpStatus.OK);
     }
 }
