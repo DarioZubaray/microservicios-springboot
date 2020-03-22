2e5304f6ffdd089dccf3bf9e19a41c57e30d89f9
dario zubaray <dzubaray@gmail.com>
2020-03-21 22:31:47 -0300

    Creando service para el CRUD y Handlers POST PUT y DELETE en el controlador servicio productos

--- springboot-servicio-productos/src/main/java/com/github/dariozubaray/springboot/productos/models/service/IProductoService.java
+++ springboot-servicio-productos/src/main/java/com/github/dariozubaray/springboot/productos/models/service/IProductoService.java
@@ -7,4 +7,6 @@ public interface IProductoService {
 
     public List<Producto> findAll();
     public Producto findById(Long id);
+    public Producto save(Producto producto);
+    public void delete(Long id);
 }
