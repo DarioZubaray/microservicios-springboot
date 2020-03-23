8cf6910ce9ca6f290e116aea42e76832307f6adc
dario zubaray <dzubaray@gmail.com>
2020-03-23 09:23:39 -0300

    agregando el proyecto commons

---
+++ springboot-servicio-commons/src/main/java/com/github/dariozubaray/springboot/commons/SpringbootServicioCommonsApplication.java
@@ -0,0 +1,8 @@
+package com.github.dariozubaray.springboot.commons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioCommonsApplication {
+
+}
---
+++ springboot-servicio-commons/src/test/java/com/github/dariozubaray/springboot/commons/SpringbootServicioCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dariozubaray.springboot.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
