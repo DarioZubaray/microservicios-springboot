faa98e14e533162364e29828a6787b548dc5de9f
dario zubaray <dzubaray@gmail.com>
2020-03-26 11:30:00 -0300

    creando el microservicioo oauth - servidor de autorizacion

---
+++ springboot-servicio-oauth/src/test/java/com/github/dariozubaray/springboot/oauth/SpringbootServicioOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dariozubaray.springboot.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
