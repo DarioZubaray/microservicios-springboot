16d91716ee2cda0b03f1fae420320011d01633db
dario zubaray <dzubaray@gmail.com>
2020-03-28 11:23:18 -0300

    correccion de las versiones springboot y spring cloud

--- springboot-servicio-usuarios/src/main/java/com/github/dariozubaray/springboot/usuarios/models/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/github/dariozubaray/springboot/usuarios/models/dao/UsuarioDao.java
@@ -8,15 +8,12 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.github.dariozubaray.springboot.usuarios.commons.models.entity.Usuario;
 
-@RepositoryRestResource(path = "usuarios")
+@RepositoryRestResource(path="usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
 
-    @RestResource(path = "buscarUsuario")
-    public Usuario findByUsername(@Param("nombre") String username);
+    @RestResource(path="buscar-username")
+    public Usuario findByUsername(@Param("username") String username);
 
-    @Query("select u from Usuario u where u.username = ?1")
-    public Usuario obtenerPorNombreusuario(String nombreusuario);
-
-    @Query(value = "select * from usuarios where username = ?1", nativeQuery = true)
-    public Usuario obtenerPorConsultaNativa(String username);
+    @Query("select u from Usuario u where u.username=?1")
+    public Usuario obtenerPorUsername(String username);
 }
--- springboot-servicio-usuarios/src/test/java/com/github/dariozubaray/springboot/usuarios/SpringbootServicioUsuariosApplicationTests.java
+++ springboot-servicio-usuarios/src/test/java/com/github/dariozubaray/springboot/usuarios/SpringbootServicioUsuariosApplicationTests.java
@@ -1,6 +1,6 @@
 package com.github.dariozubaray.springboot.usuarios;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
