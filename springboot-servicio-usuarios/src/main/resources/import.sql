8fec62a323e5690d37667100677787dbbd1b700e
dario zubaray <dzubaray@gmail.com>
2020-03-30 12:01:01 -0300

    implementando base de datos postgresql para microservicio usuario

--- springboot-servicio-usuarios/src/main/resources/import.sql
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$.54WeDAAZmi/oyu1RBeH2eXRaaKcXN1hufY04B.6HgXrb7O48k7t2', 1, 'DJ', 'Zubaray', 'djzubaray@gmail.com')
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('dario', '$2a$10$MDMyDuoy9zIWOakRlfPmiuB8B7OJe7g2e0tmxu5tZn.PCAR.M2Aou', 1, 'Dario', 'Zubaray', 'dzubaray@gmail.com')
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('julieta', '$2a$10$VmKn.LUCZ2FHC29n2dhe8uXIzh3VJa.8EfuuSVvUOPoQ8o05N6Yge', 1, 'Julieta', 'Zubaray', 'jzubaray@gmail.com')
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('valentina', '$2a$10$5d5Q9t7rzWbrpktgLJHjTeAN.mTrbVD54S5l/.umLaqW0P4jGtBKm', 1, 'JV', 'Zubaray', 'jvzubaray@gmail.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$.54WeDAAZmi/oyu1RBeH2eXRaaKcXN1hufY04B.6HgXrb7O48k7t2', true, 'DJ', 'Zubaray', 'djzubaray@gmail.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('dario', '$2a$10$MDMyDuoy9zIWOakRlfPmiuB8B7OJe7g2e0tmxu5tZn.PCAR.M2Aou', true, 'Dario', 'Zubaray', 'dzubaray@gmail.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('julieta', '$2a$10$VmKn.LUCZ2FHC29n2dhe8uXIzh3VJa.8EfuuSVvUOPoQ8o05N6Yge', true, 'Julieta', 'Zubaray', 'jzubaray@gmail.com')
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('valentina', '$2a$10$5d5Q9t7rzWbrpktgLJHjTeAN.mTrbVD54S5l/.umLaqW0P4jGtBKm', true, 'JV', 'Zubaray', 'jvzubaray@gmail.com')
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER')
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN')
